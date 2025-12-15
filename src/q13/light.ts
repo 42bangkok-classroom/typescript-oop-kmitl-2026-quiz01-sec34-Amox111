9c4f6d751797c88ebb274b3d9c7423560e974faf
Amox111 <ruxkwarmmana233@gmail.com>
2025-12-15 17:36:21 +0700

    n

---
+++ src/q13/light.ts
@@ -0,0 +1,15 @@
+const light = String(process.argv[2]);
+
+switch (light.toLocaleLowerCase()){
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Caution");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Unknow");
+}
